024e5ee860d71a94ca7d81d632373b5129a490dd
Veteran0411 <somlinghajji@gmail.com>
2025-04-17 16:17:02 +0530

    init commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+class Model {
+    convert(value, direction) {
+        if (direction === "CtoF") {
+            return (value * 9 / 5) + 32;
+        } else if (direction === "FtoC") {
+            return (value - 32) * 5 / 9;
+        }
+    }
+}
+
+class View{
+    constructor(){
+        this.tempInput=document.getElementById("tempInput");
+        this.convertBtn=document.getElementById("convertBtn");
+        this.convertDirection = document.getElementById("convertDirection");
+        this.result = document.getElementById("result");
+    }
+
+    bindConvert(handler){
+        this.convertBtn.addEventListener('click',()=>{ 
+            // if i write normal function then it will be refering to button,
+            // but  arrow function it will refer this to where it has been defined i.e defined of this.tempInput
+            const tempValue=parseInt(this.tempInput.value);
+            const direction=this.convertDirection.value;
+            handler(tempValue,direction);
+        })
+    }
+
+    displayResult(value){
+        if (typeof value === "number") {
+            this.result.textContent = `Result is ${value.toFixed(2)}`;
+        } else {
+            this.result.textContent = value;
+        }
+    }
+}
+
+class Controller{
+    constructor(model,view){
+        this.model=model;
+        this.view=view;
+
+        this.view.bindConvert(this.handleConversion.bind(this));
+    }
+
+    handleConversion(value,direction){
+        if(isNaN(value)){
+            this.view.displayResult("Please enter valid number")
+        }
+        const result=this.model.convert(value,direction);
+        this.view.displayResult(result);
+    }
+}
+
+const app = new Controller(new Model(), new View());
